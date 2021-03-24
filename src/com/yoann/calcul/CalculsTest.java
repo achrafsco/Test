e52da515f67a00931a6744d97ecd96e22d605d0b
test <test@EPSI-5>
2021-03-24 19:38:07 +0100

    Add test

---
+++ src/com/yoann/calcul/CalculsTest.java
@@ -0,0 +1,14 @@
+package com.yoann.calcul;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
