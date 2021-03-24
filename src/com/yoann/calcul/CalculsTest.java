dc37a5c1da0f68006ea129e9e41f3d38271dbf5c
test <test@EPSI-5>
2021-03-24 19:43:23 +0100

    test for file on FS

--- src/com/yoann/calcul/CalculsTest.java
+++ src/com/yoann/calcul/CalculsTest.java
@@ -2,13 +2,22 @@ package com.yoann.calcul;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 
 class CalculsTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		if (new File("C:\\Users\\test\\Desktop\\apache-tomcat-9.0.14-windows-x64\\apache-tomcat-9.0.14\\logs\\catalina.2021-03-24.log").exists())
+		{
+			System.out.println("Found file for tomcat logs");
+		}
+		else
+		{
+			fail("Not yet implemented");
+		}
 	}
 
 }
