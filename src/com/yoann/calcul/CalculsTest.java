5b7b000018304effc3f9dce055413c04d6be5686
test <test@EPSI-5>
2021-03-25 09:15:00 +0100

    update empty test

--- src/com/yoann/calcul/CalculsTest.java
+++ src/com/yoann/calcul/CalculsTest.java
@@ -4,20 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculsTest {
 
+	@BeforeEach
+	public void initTest()
+	{
+		Calculs c = new Calculs(5,10);
+	}
+	
+	
 	@Test
-	void test() {
-		if (new File("C:\\Users\\test\\Desktop\\apache-tomcat-9.0.14-windows-x64\\apache-tomcat-9.0.14\\logs\\catalina.2021-03-24.log").exists())
-		{
-			System.out.println("Found file for tomcat logs");
-		}
-		else
-		{
-			fail("Not yet implemented");
-		}
+	public void test() 
+	{
+		
+		
 	}
 
 }
