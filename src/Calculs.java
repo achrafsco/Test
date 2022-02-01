01a43788f8ec7c9a86f125e01e39b3452d342ce4
Achraf <afriachraf@gmail.com>
2022-02-01 16:17:42 +0100

    second commit

--- src/Calculs.java
+++ src/Calculs.java
@@ -1,4 +1,4 @@
-package com.yoann.calcul;
+
 public class Calculs
 {
         private final int operande1;
--- test/CalculsTest2.java
+++ test/CalculsTest2.java
@@ -1,6 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.yoann.calcul.Calculs;
+
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
